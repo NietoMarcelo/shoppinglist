746bb3d79ffed039e93a5ab2b54463c7e092fd74
Chycho <ivan_zagarra@hotmail.com>
2022-08-27 15:39:12 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Another something
+          Godines
         </a>
       </header>
     </div>
